f1a52f884ac214fc08c6f7b542f04048e97d06aa
Miriam DeAna <miriam.williams@careerbuilder.com>
2013-02-16 20:01:08 -0500

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Morepls::Application.config.secret_token = '12a7063e926c10cb0f1c6bac1b92f3af83815308673e42f4bdc1d364c45f8a86df347fdd6b89d24a74fe7eec986b2e2e7a31a2c26267749b01d31aa67a3ba790'
